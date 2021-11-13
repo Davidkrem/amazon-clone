6b1c1b9687b5f49c638d0ae523ae4816a7f11308
David Kremenitzer <dkrem21@gmail.com>
2021-11-13 13:48:19 -0500

    add Order functionality and firebase bug fix

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './CheckoutProduct.css';
 import { useStateValue } from './StateProvider';
 
-function CheckoutProduct({ id, image, title, price, rating }) {
+function CheckoutProduct({ id, image, title, price, rating, hideButton }) {
   const [{ cart }, dispatch] = useStateValue();
 
   const removeFromBasket = () => {
@@ -12,23 +12,25 @@ function CheckoutProduct({ id, image, title, price, rating }) {
     });
   };
   return (
-    <div className="checkoutProduct">
-      <img className="checkoutProduct__image" src={image} alt="" />
+    <div className='checkoutProduct'>
+      <img className='checkoutProduct__image' src={image} alt='' />
 
-      <div className="checkoutProduct__info">
-        <p className="checkoutProduct__title">{title}</p>
-        <p className="checkoutProduct__price">
+      <div className='checkoutProduct__info'>
+        <p className='checkoutProduct__title'>{title}</p>
+        <p className='checkoutProduct__price'>
           <small>$</small>
           <strong>{price}</strong>
         </p>
-        <div className="checkoutProduct__rating">
+        <div className='checkoutProduct__rating'>
           {Array(rating)
             .fill()
             .map((_, i) => (
               <p>⭐</p>
             ))}
         </div>
-        <button onClick={removeFromBasket}>Remove from Cart</button>
+        {!hideButton && (
+          <button onClick={removeFromBasket}>Remove from Cart</button>
+        )}
       </div>
     </div>
   );
